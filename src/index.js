be056addacfddbc1499a781822bcdbb925803d79
Anjali yadav <110176232+Anjaliy17@users.noreply.github.com>
2022-08-19 23:39:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import App from "./components/App";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
+function myFunction(value){
+    console.log(value);
+document.getElementById('container').innerHTML=value;
+}
